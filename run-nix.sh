42087d646d84dc78b2f37e249203aeab6a8a8bb3
Raj Sahae <rsahae@tesla.com>
2018-03-22 15:23:35 -0700

    Get linux working

--- run-nix.sh
+++ run-nix.sh
@@ -1,3 +1,16 @@
 #!/bin/bash
 
-docker run --rm -v /tmp/.X11-unix:/tmp/.X11-unix -e DISPLAY=$DISPLAY rajsahae/fxruby:latest
+XSOCK="/tmp/.X11-unix"
+XAUTH=$(mktemp /tmp/.Xauth.docker.XXXX)
+xauth nlist "$(hostname)$DISPLAY" | sed -e 's/^..../ffff/' | xauth -f "$XAUTH" nmerge -
+chmod 666 "$XAUTH"
+
+docker run \
+  --rm \
+  -v "$XSOCK:$XSOCK" \
+  -v "$XAUTH:$XAUTH" \
+  -e "XAUTHORITY=$XAUTH" \
+  -e DISPLAY \
+  rajsahae/fxruby:latest
+
+rm -f "$XAUTH"
